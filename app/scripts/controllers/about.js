fa7f5c0bbf42b12014213d22010eb8e25233c61a
Bojan Janevski <bojan@janevski.info>
2018-03-21 12:35:49 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magazineApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the magazineApp
+ */
+angular.module('magazineApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magazineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the magazineApp
+ */
+angular.module('magazineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
